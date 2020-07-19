c33e2e0b8f4f9b43288ecf129332222aa9302bee
Ruan Luies <omega@live.co.za>
2020-07-19 13:13:00 +0200

    fix: remove download_url, use pypi.

--- setup.py
+++ setup.py
@@ -17,13 +17,12 @@ def read(fname):
 setup(
   name = 'iotdatalog',
   packages = ['iotdatalog'],
-  version = '0.1.4.2',
+  version = '0.1.5',
   license='GPL',
   description = 'Wrapper for datalog.anglebrackets.co.za REST API',
   author = 'Ruan Luies',
   author_email = 'omega@live.co.za',
   url = 'https://github.com/baggins800/iotdatalog',
-  download_url = 'https://github.com/baggins800/iotdatalog/archive/iotdatalog-0.1.tar.gz',
   keywords = ['IOT', 'datalogging', 'datalog', 'datalogger'],
   install_requires=[  
     'requests>=1.6', 'responses', 'datetime'
